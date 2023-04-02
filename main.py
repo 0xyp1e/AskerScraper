bce304a52c50111b62c0edca8507950e1dd216d6
0xyp1e <0xyp1e@proton.me>
2023-04-02 00:07:12 -0300

    Getting questions

---
+++ main.py
@@ -0,0 +1,77 @@
+import requests
+from bs4 import BeautifulSoup
+from question import Question
+import json
+
+'''
+Gets a question's id from a li question item.
+'''
+def getq_id(liq):
+    return int(liq["data-id"])
+
+'''
+Gets the user's username who asked a question from a li question item.
+'''
+def getq_username(liq):
+    return liq.find("span", class_="qcreator-small").string.strip()
+
+'''
+Gets the question's title from a li question item.
+'''
+def getq_title(liq):
+    return liq.find("a", class_="q-title").text.strip()
+
+'''
+Transforms a liq into a Question obj.
+'''
+def transformliq(liq):
+    return Question(getq_id(liq), getq_title(liq), getq_username(liq))
+
+'''
+Get 'n' questions.
+'''
+def get_questions(n, fid = 0):
+    questions = []
+
+    getted = 0
+    nextfqid = fid
+
+    while getted < n:
+        # Getting questions
+        url = f"https://asker.fun/more_questions?maxid={nextfqid}&order=c&cat=0&max_questions=1"
+        html = requests.get(url).text
+
+        # "Souping it"
+        soup = BeautifulSoup(html, "html.parser")
+
+        # Getting questions links
+        questions_links = soup.find_all("li", class_="list-group-item")
+
+        # Retrieving only remaining questions
+        questions_links = questions_links[0:n - getted]
+        
+        # Saving them
+        for q in questions_links:
+            question = transformliq(q)
+            questions.append(question)
+
+        # Getting next question's id to retrieve
+        nextfqid = getq_id(questions_links[len(questions_links) - 1]) - 1
+
+        # Increasing number of getted questions
+        getted += len(questions_links)
+        print(f"Getted {getted} questions!")
+    
+    print()
+    return questions
+
+'''
+App's entry point.
+'''
+def main(n, fid = 0):
+    questions = get_questions(n, fid)
+
+    # Printing all questions in JSON format:
+    print(json.dumps([q.__dict__ for q in questions]))
+
+main(10, 0)
---
+++ question.py
@@ -0,0 +1,17 @@
+import json
+
+class Question:
+    # Question's constructor
+    def __init__(self, id, title, username):
+        self.id = id
+
+        self.title = title
+        self.username = username
+    
+    # Showing attributes (in pretty format)
+    def pshow(self):
+        print(30 * "=")
+        print(f"ID: {self.id}\nTITLE: {self.title}\nUSER: {self.username}")
+
+    def jshow(self):
+        print(json.dumps(self.__dict__) + ",")
